ec21d91e3d9893b58fcc3c5c3ab973d37a317247
Moin Khan <moin6k@gmail.com>
2023-09-19 17:46:54 +0530

    lambda function practice code added

---
+++ lambaFunction.py
@@ -0,0 +1,16 @@
+# def double(x):
+#     return x*2
+
+# print(double(5)) This is normal function
+
+
+#Lamda Function: 
+double = lambda x: x*2
+
+print(double(5))
+
+
+# def square(fx,v):
+#     return 10 + fx(v)
+
+# print(square(double,5))   WE CAN PASS FUNCTION AS ARGUMENT
