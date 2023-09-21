7d0ca64d0cfdf92967b1a82d2b3fb832bb943771
Moin Khan <moin6k@gmail.com>
2023-09-22 00:08:28 +0530

    shutil module practice code added

---
+++ shutilModule.py
@@ -0,0 +1,23 @@
+import shutil
+import os
+
+# shutil.copy("shutilModule.py","shutilModuleCopyFunctionUsed.py")
+# shutilModuleCopyFunctionUsed.py this class will be having the same content as shutilModule.py
+
+# shutil.copytree("pdf","pdfcopy")
+# pdfcopy this folder will be having the same content as pdf
+
+# shutil.move("pdf/lion.pdf","clutterfolder/lion.pdf")
+# pdfcopy/lion.pdf file will move to the clutterfolder folder
+
+# shutil.rmtree("clutterfolder")
+# It will delete the folder
+
+# os.remove("fileaddress")
+# This will remove the file
+
+
+
+
+
+
